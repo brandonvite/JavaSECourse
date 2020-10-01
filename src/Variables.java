05bb2eaa81e1f1bdddfe292792544bee27352ba7
Brandon Vite <bramsel.vite@gmail.com>
2020-10-01 13:46:37 -0500

    Actualizando Variables

---
+++ src/Variables.java
@@ -0,0 +1,13 @@
+public class Variables {
+    public static void main(String[] args) {
+        //Declarando un variable
+        int speed;
+        //Asignamos un valor a una variable
+        speed = 10;
+        System.out.println(speed);
+
+        int salary = 1000;
+        String employeeName = "Brandon Vite";
+        System.out.println(employeeName);
+    }
+}
