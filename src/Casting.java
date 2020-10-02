54345ee126cadac808d0c175f235715e23d7570a
Brandon Vite <bramsel.vite@gmail.com>
2020-10-02 13:27:23 -0500

    tag 2.10

--- src/Casting.java
+++ src/Casting.java
@@ -11,10 +11,21 @@ public class Casting {
         int estimatedMonthlyDogs = (int)monthlyDogs;
         System.out.println(estimatedMonthlyDogs);
 
+
         //Exactitud
         int a = 30;
         int b = 12;
 
         System.out.println((double) a/b);
+
+        double c = (double) a/b;
+        System.out.println(c);
+
+        char n = '1';
+        int nI = n;
+        System.out.println(nI);
+
+        short nS = (short) n;
+        System.out.println(nS);
     }
 }
